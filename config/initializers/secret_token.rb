f2f11d8be90715d145b7cf2798a300299a66641f
Guillermo Correa <guille_freelance@hotmail.com>
2011-06-06 13:38:32 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ridemo::Application.config.secret_token = '55f867ea1d1682fdf120930cf0c08e0d648d9100e95613bb2ec19edfcbf01440d9335ab816c84a6488299eab56f55e84e5cafbd044b0a57bf8456a561209cc61'
